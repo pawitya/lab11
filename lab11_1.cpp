527016de377c315df02da721b872f66838191876
pawitchaya <boss@cmu.ac.th>
2024-12-23 20:53:15 +0700

    lap11_1.cpp completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,20 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+    srand(time(0));
+    string g[] = {"A" , "B+" , "B" , "C+" , "C" , "D+" , "D" , "F" , "W"};
+    cout << "Press Enter 3 times to reveal your future.";
+    
+    for (int i = 0; i < 3; i++){
+        cin.get();
+    }
+    
+    int random = rand() % 9;
+    string rangrade = g[random];
+    cout << "You will get " << rangrade << " in this 261102. ";
+    
+    return 0;
+}
